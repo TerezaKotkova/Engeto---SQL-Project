42c935dcdc481d7deb0f7e70bc1d784476e37557
TerezaKotkova <116884320+TerezaKotkova@users.noreply.github.com>
2022-11-26 20:23:42 +0100

    Create Primary Table  - SQL Project  copy.sql
    Basic research of necessary data from available database tables.

---
+++ Primary Table  - SQL Project  copy.sql
@@ -0,0 +1,22 @@
+SELECT *
+FROM czechia_payroll;
+
+SELECT *
+FROM czechia_payroll_unit;
+
+SELECT *
+FROM czechia_payroll_value_type;
+
+SELECT *
+FROM czechia_payroll_calculation;
+
+SELECT *
+FROM czechia_price;
+
+SELECT *
+FROM czechia_price_category;
+
+
+
+
+
