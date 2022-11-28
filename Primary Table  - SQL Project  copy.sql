677f0a2cb1cb708d1f5d47a56d515088ccfc7448
TerezaKotkova <116884320+TerezaKotkova@users.noreply.github.com>
2022-11-28 12:27:55 +0100

    Update Primary Table  - SQL Project  copy.sql
    Count of all values = 3784,
    Count of all values with  NULL rows = 3784,
    Count of employee amount = 1720 rows,
    Count of employee amount with null  = 1720,
    Count of FTE = same result as employee amount

--- Primary Table  - SQL Project  copy.sql
+++ Primary Table  - SQL Project  copy.sql
@@ -18,5 +18,44 @@ FROM czechia_price_category;
 
 
 
+SELECT 	
+	count(value)
+FROM czechia_payroll;
+
+
+SELECT 	
+	count(value)
+FROM czechia_payroll
+WHERE value IS NOT NULL;
+
+
+SELECT 	
+	count(value)
+FROM czechia_payroll
+WHERE value IS NOT NULL
+	AND value_type_code = '5958'
+	AND calculation_code  = '100';
+
+
+SELECT 	
+	count(value)
+FROM czechia_payroll
+WHERE value_type_code = '5958'
+	AND calculation_code  = '100';
+
+	
+SELECT 	
+	count(value)
+FROM czechia_payroll
+WHERE value IS NOT NULL
+	AND value_type_code = '5958'
+	AND calculation_code  = '200';
+
+
+SELECT 	
+	count(value)
+FROM czechia_payroll
+WHERE value_type_code = '5958'
+	AND calculation_code  = '200';
 
 
