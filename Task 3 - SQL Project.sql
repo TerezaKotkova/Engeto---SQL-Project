bcfc0236944c04a44c079c63b3dd0c4b32ccc0e7
TerezaKotkova <116884320+TerezaKotkova@users.noreply.github.com>
2022-11-30 11:07:13 +0100

    Update Task 3 - SQL Project.sql
    Update Task 3 - FROM -  deleted space

--- Task 3 - SQL Project.sql
+++ Task 3 - SQL Project.sql
@@ -98,6 +98,6 @@ WHERE percentage_price_diff IN(
 SELECT 
 	name,
 	sum(percentage_price_diff) AS total_percentage_diff
-FROM t_ price_percentage_difference
+FROM t_price_percentage_difference
 GROUP BY name
 ORDER BY sum(percentage_price_diff);
