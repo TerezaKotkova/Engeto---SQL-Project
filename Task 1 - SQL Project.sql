4800f85bac9d3c618d946e15d007c0176f6d9a0b
TerezaKotkova <116884320+TerezaKotkova@users.noreply.github.com>
2022-11-30 15:18:38 +0100

    add View, percentage difference
    Create view and new column with gross wage  differences between years in %.

--- Task 1 - SQL Project.sql
+++ Task 1 - SQL Project.sql
@@ -54,3 +54,33 @@ FROM (
 	) AS b 
 	ON a.industry_branch = b.industry_branch AND a.YEAR = b.YEAR - 1
 ORDER BY diff_gross_wages;
+
+
+CREATE OR REPLACE VIEW v_percentage_price_difference AS 
+SELECT 
+	a.industry_branch AS industry_branch,
+	a.YEAR AS year,
+	a.avg_gross_wage AS avg_gross_wage,
+	b.industry_branch AS industry_branch2,
+	b.YEAR AS year2,
+	b.avg_gross_wage AS avg_gross_wage2,
+	round((b.avg_gross_wage - a.avg_gross_wage),0) AS diff_gross_wages_in_kc,
+	round(((b.avg_gross_wage - a.avg_gross_wage) / a.avg_gross_wage) * 100, 2) AS percentage_gross_wage_diff
+FROM (
+	SELECT 
+		t1.industry_branch,
+		t1.YEAR,
+		t1.avg_gross_wage
+	FROM t_tereza_kotkova_project_SQL_primary_final AS t1
+	GROUP BY t1.industry_branch, t1.year
+	) AS a JOIN (
+	SELECT 
+		t2.industry_branch,
+		t2.YEAR,
+		t2.avg_gross_wage
+	FROM t_tereza_kotkova_project_SQL_primary_final AS t2
+	GROUP BY t2.industry_branch, t2.YEAR
+	) AS b 
+	ON a.industry_branch = b.industry_branch AND a.YEAR = b.YEAR - 1
+ORDER BY percentage_gross_wage_diff;
+
